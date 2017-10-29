24ef26b202bdcf30fbe33ee24eb5cc818c34b4b5
sam bolton <info@sambolton.me>
2017-10-29 21:27:26 +0000

    minor changes to route || require mongoose

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 var port = 8000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(express.static("public"));
 
 app.post('/api/send/submissions', function (req, res) {
-	var formSub = req.param('formSub'); 
+	var formSub = req.body.formSub; 
 	console.log("Request recieved " + formSub);
-	res.send("Recieved!")
-	
-})
+	res.send("Recieved!")	
+});
 
 app.listen(port);

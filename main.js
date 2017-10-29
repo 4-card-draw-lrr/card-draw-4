f940fff2e5f411040a260024a2312a4e29cace2c
sam bolton <info@sambolton.me>
2017-10-29 17:57:28 +0000

    add post route|incomplete

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 var port = 8000;
 
-
+app.post('/api/send/submissions', function (req, res) {
+	var formSub = req.param('formSub'); 
+	console.log("Request recieved " + formSub);
+	res.send("Recieved!")
+	
+})
 
 app.listen(port);

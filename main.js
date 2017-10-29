f615f2fc124122174bece5b09c55a4c5b13e024f
sam bolton <info@sambolton.me>
2017-10-29 21:31:34 +0000

    define submission schema

--- main.js
+++ main.js
@@ -8,6 +8,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 app.use(express.static("public"));
 
+var deckSchema = new Schema({
+	"name":String,
+	"card1":String,
+	"card2":String,
+	"card3":String,
+	"card4":String,
+	"deckName":String
+});
+
 app.post('/api/send/submissions', function (req, res) {
 	var formSub = req.body.formSub; 
 	console.log("Request recieved " + formSub);
